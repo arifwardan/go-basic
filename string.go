b9276348ae36ee78c003c5dd4ea25323f69753bb
arifwardan <arifwardan.id@gmail.com>
2020-08-23 20:42:26 +0700

    ~

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Arif")
+	fmt.Println("Arif Wardan")
+	fmt.Println("Arif Wardan 20")
+
+	fmt.Println(len("Arif"))
+	fmt.Println("Arif Wardan"[0])
+	fmt.Println("Arif Wardan 20"[1])
+}

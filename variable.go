84dd09bb2480b3b1ae6c371bb06aebdac796d82a
arifwardan <arifwardan.id@gmail.com>
2020-08-24 08:33:15 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+var age int
+
+func main() {
+	var name = "Arif Wardan"
+	fmt.Println(name)
+
+	age = 20
+	fmt.Println(age)
+
+	friendName := "andi"
+	fmt.Println(friendName)
+
+	var(
+		firstName = "Arif"
+		lastName  = "Wardan"
+		myAge     = 20
+		float 	  = true
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(myAge)
+	fmt.Println(float)
+}

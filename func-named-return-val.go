003bd6dc2f34a1ac397993f29a7c214903b99d3a
arifwardan <arifwardan.id@gmail.com>
2020-09-11 09:10:08 +0700

    func named return val

--- func-named-return-val.go
+++ func-named-return-val.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+/*
+	jika tipe data yg pertama dan kedua tidak di deklarasikan
+	maka tipe data terakhir wajib, dan dijadikan default
+	perhatikan function di bawah
+*/
+func getName() (firstName, middleName string, age int16 ){
+	firstName = "Arif"
+	middleName = "Wardan"
+	age = 20
+	return
+}
+
 func main() {
-	$END$
+	fmt.Println(getName())
+	a, b, c := getName()
+	fmt.Println("first name : ",a )
+	fmt.Println("last name : ",b )
+	fmt.Println("umur : ",c )
 }

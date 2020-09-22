afd7174e2d12a66d2574d00319f4f84e7a886d5c
arifwardan <arifwardan.id@gmail.com>
2020-09-22 22:37:01 +0700

    variadic with slice

--- function-variadic.go
+++ function-variadic.go
@@ -13,4 +13,8 @@ func sumAll(numbers ...int) int {
 func main() {
 	total := sumAll(23,32,34,5,34,567,8,)
 	fmt.Println(total)
+
+	slice := []int{23,54,76,34,76}
+	total = sumAll(slice...)
+	fmt.Println(total)
 }
